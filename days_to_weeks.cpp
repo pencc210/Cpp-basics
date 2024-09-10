97af3c3235d10c70c1c742eb0ca3fb7a26cf3970
pencc210 <pcer210@gmail.com>
2024-09-10 13:59:08 -0300

    Days into weeks calculator
    Converts a certain number of days into weeks plus remaining days

---
+++ days_to_weeks.cpp
@@ -0,0 +1,23 @@
+
+/* This program takes a number of days form a user and outputs the equivalent
+number of weeks + extra days*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int days;
+    int x_days;
+    int weeks;
+
+    cout<<"Please enter the number of days: "<<endl;
+    cin>>days;
+
+    weeks = days/7;
+    x_days = days%7;
+
+    cout<<weeks<<" weeks and "<<x_days<<" days."<<endl;
+
+
+}
